20211d8ee96bc19720723dc5992e27ce49b44a22
Marco Guerreiro <marcotulio.guerreiro@gmail.com>
2026-04-19 12:47:05 +0200

    Initial build for cpu1

--- vitis/plat_create_build.py
+++ vitis/plat_create_build.py
@@ -13,11 +13,16 @@ from dataclasses import dataclass, field
 
 @dataclass
 class AppData:
-    name    : str = 'app'
-    os      : str = 'standalone'
-    template: str = 'hello_world'
-    cmake   : dict = field(default_factory=dict)
-    options : dict = field(default_factory=dict)
+    processor   : str = ''
+    domain_name : str = ''
+    domain      : str = ''
+    app         : str = ''
+    app_name    : str = 'app'
+    os          : str = 'standalone'
+    template    : str = 'hello_world'
+    cmake       : dict = field(default_factory=dict)
+    options     : dict = field(default_factory=dict)
+
 
 # --- Logging config ---
 logger = logging.getLogger(__name__)
@@ -46,6 +51,9 @@ args = parser.parse_args()
 ws = args.ws
 json_cfg = args.config
 
+cpu0 = None
+cpu1 = None
+
 # --- Config file processing ---
 if json_cfg:
     with open(json_cfg, mode='r') as f:
@@ -56,32 +64,40 @@ if json_cfg:
 
     if 'cpu0' in jdata:
         cpu0 = AppData(**jdata['cpu0'])
+        cpu0.domain_name = 'cpu0'
+        cpu0.processor = 'ps7_cortexa9_0'
 
+    if 'cpu1' in jdata:
+        cpu1 = AppData(**jdata['cpu1'])
+        cpu1.domain_name = 'cpu1'
+        cpu1.processor = 'ps7_cortexa9_1'
+
+cpus = [cpu0, cpu1]
 
 # --- Process cmake options ---
 def process_cpu_cmake(cpu, ws):
     if 'config' in cpu.cmake:
-        pathlib.Path(f"{ws}/{cpu.name}/src/UserConfig.cmake").unlink()
-        f = shutil.copy(f"{cpu.cmake['config']}", f'{ws}/{cpu.name}/src')
-        pathlib.Path(f).rename(f"{ws}/{cpu.name}/src/UserConfig.cmake")
+        pathlib.Path(f"{ws}/{cpu.app_name}/src/UserConfig.cmake").unlink()
+        f = shutil.copy(f"{cpu.cmake['config']}", f'{ws}/{cpu.app_name}/src')
+        pathlib.Path(f).rename(f"{ws}/{cpu.app_name}/src/UserConfig.cmake")
 
     add_text = ""
-    if 'path_vars' in cpu0.cmake:
+    if 'path_vars' in cpu.cmake:
         add_text += "\n"
-        for v, p in cpu0.cmake['path_vars'].items():
+        for v, p in cpu.cmake['path_vars'].items():
             path = pathlib.Path(p)
             p = p if path.is_absolute() is True else path.resolve()
             add_text += f"set({v} \"{p}\")\n"
 
-    if 'include' in cpu0.cmake:
+    if 'include' in cpu.cmake:
         add_text += "\n"
         add_text += f"include(\"{cpu.cmake['include']}\")\n"
 
     if add_text != "":
-        with open(f"{ws}/{cpu.name}/src/UserConfig.cmake", "r") as f:
+        with open(f"{ws}/{cpu.app_name}/src/UserConfig.cmake", "r") as f:
             lines = f.readlines()
 
-        with open(f"{ws}/{cpu.name}/src/UserConfig.cmake", "w") as f:
+        with open(f"{ws}/{cpu.app_name}/src/UserConfig.cmake", "w") as f:
             end_section = "###   END OF USER SETTINGS SECTION ###"
             for line in lines:
                 if end_section in line:
@@ -101,7 +117,6 @@ def process_lib_params(domain, libs):
             domain.set_lib(lib)
         for p, v in libs[lib].items():
             domain.set_config(option='lib', param=p, value=v, lib_name=lib)
-
 
 # --- Workspace and projects ---
 cpu0_name = 'cpu0'
@@ -123,31 +138,31 @@ if plats == []:
 
     fsbl = platform.get_domain('zynq_fsbl')
 
-    platform.add_domain(name=cpu0_name, cpu='ps7_cortexa9_0', os=cpu0.os)
-    cpu0_domain = platform.get_domain(cpu0_name)
+    for cpu in cpus:
+        if cpu is None:
+            continue
+                    
+        platform.add_domain(name=cpu.domain_name, cpu=cpu.processor, os=cpu.os)
+        cpu.domain = platform.get_domain(cpu.domain_name)
 
-    if cpu0.options:
-        if 'os' in cpu0.options:
-            process_os_params(cpu0_domain, cpu0.options['os'])
+        if cpu.options:
+            if 'os' in cpu.options:
+                process_os_params(cpu.domain, cpu.options['os'])
 
-        if 'lib' in cpu0.options:
-            process_lib_params(cpu0_domain, cpu0.options['lib'])
-            
-##    if cpu1_app_name is not None:
-##        platform.add_domain(name=cpu1_name, cpu='ps7_cortexa9_1', os='standalone')
-##        cpu1_domain = platform.get_domain(cpu1_name)
+            if 'lib' in cpu.options:
+                process_lib_params(cpu.domain, cpu.options['lib'])
     
     platform.build()
     platform_xpfm = client.find_platform_in_repos(platform_name)
 
-    cpu0_app = client.create_app_component(name=cpu0.name, platform=platform_xpfm, domain=cpu0_name, template=cpu0.template)
-    if cpu0.cmake:
-        process_cpu_cmake(cpu0, ws)
+    for cpu in cpus:
+        if cpu is None:
+            continue
 
-##    if cpu1_app_name is not None:
-##        cpu1_app = client.create_app_component(name=cpu1_app_name, platform=platform_xpfm, domain=cpu1_name, template='hello_world')
-##    else:
-##        cpu1_app = None
+        cpu.app = client.create_app_component(name=cpu.app_name, platform=platform_xpfm, domain=cpu.domain_name, template=cpu.template)
+        if cpu.cmake:
+            process_cpu_cmake(cpu, ws)
+
 else:
     # Loads the existing platform
     platform_path = plats[0].platform_location
@@ -161,28 +176,24 @@ else:
     platform = client.get_component(platform_name)
     platform.build()
 
-    cpu0_domain = platform.get_domain(cpu0_name)
-    cpu0_app = client.get_component(cpu0.name)
-##    if cpu1_app_name is not None:
-##        cpu1_app = client.get_component(cpu1_app_name)
-##    else:
-##        cpu1_app = None
+    for cpu in cpus:
+        if cpu is None:
+            continue
 
+        cpu.domain = platform.get_domain(cpu.domain_name)
+        cpu.app = client.get_component(cpu.app_name)
+        if cpu.options:
+            if 'os' in cpu.options:
+                process_os_params(cpu.domain, cpu.options['os'])
 
-    if cpu0.options:
-        if 'os' in cpu0.options:
-            process_os_params(cpu0_domain, cpu0.options['os'])
-
-        if 'lib' in cpu0.options:
-            process_lib_params(cpu0_domain, cpu0.options['lib'])
-
+            if 'lib' in cpu.options:
+                process_lib_params(cpu.domain, cpu.options['lib'])
 
 # --- Build applications ---
-cpu0_app.clean()
-cpu0_app.build()
-
-##if cpu1_app_name is not None:
-##    cpu1_app.clean()
-##    cpu1_app.build()
+for cpu in cpus:
+    if cpu is None:
+        continue
+    cpu.app.clean()
+    cpu.app.build()
 
 vitis.dispose()
